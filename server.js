1ed9d847e2c26eb4715bc546973c9b1ed4e1d7aa
kobyl <koby@pipingcold.com>
2022-04-09 00:35:35 -0700

    Add kook header

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Listen on a specific host via the HOST environment variable
 var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8081;
 
 // Grab the blacklist from the command-line so that we can update the blacklist without deploying
 // again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
@@ -26,8 +26,8 @@ cors_proxy.createServer({
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
-    'cookie',
-    'cookie2',
+    // 'cookie',
+    // 'cookie2',
     // Strip Heroku-specific headers
     'x-request-start',
     'x-request-id',
